c7770f8f3b79c937009ebc5f95477b04967c8305
ayeshaafzal1573 <ayeshaafzal1573@gmail.com>
2024-06-23 23:54:20 +0500

    login-endpoint-success

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 //json web token client or server k bech m security provide krta ha
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'Aishaisprogrammer';
-//CREATE A USER USING: POST "/api/auth/". no login require
+//CREATE A USER END POINT USING: POST "/api/auth/". no login require
 router.post('/createuser', [
     body('name', 'Enter a valid Name').isLength({ min: 3 }),
     body('email', 'Enter a valid Email').isEmail(),
@@ -56,10 +56,42 @@ router.post('/createuser', [
     //     console.log(err)
     //     res.json({ error: 'Please enter a unique value for email', message: err.message })
     // })
+});
+//Authenticate a user using:POST "api/auth/login"
+router.post('/login', [
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
 
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 
 
 });
-
 
 module.exports = router;
